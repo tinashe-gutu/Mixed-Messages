71b1b1b44094bedabeef8206df721d38bab9d8c7
tinashe-gutu <tinashegutu2011@gmail.com>
2022-03-13 19:47:24 +0000

    setting random number to change depending on array length

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,19 +1,21 @@
 const mixedMessages = {
     time: [['Today'], ['Tomorrow'], ['This Week'], 
-            ["January 29th"], ["At this Exact Moment"]],
+            ["January 29th,"], ["At this Exact Moment"]],
     affirmative: [['you will'],['you wont'], ['you might'], ["you are certain to"] ],
     event: [['fall in love.'], ['recieve a once in a life time offer.'], ['get arrested.']]
 }
 
-const randomNum = () => {
-    num = Math.floor((Math.random())*3)
-    return num
+const randomNum = (num) => {
+    const randomNum = Math.floor((Math.random())*num)
+    return randomNum
 }
 
 const createMessage = () =>{
     const arrOfMessage = []
     for(messagePart in mixedMessages){
-        arrOfMessage.push(mixedMessages[messagePart][randomNum()])
+        const randomNm=randomNum(mixedMessages[messagePart].length)
+        const randomMsg=mixedMessages[messagePart][randomNm];
+        arrOfMessage.push(randomMsg);
     }
     return arrOfMessage.join(' ');
 }
