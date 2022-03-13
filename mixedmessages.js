f18fa16366b4843a510c60c023092fa178192c9a
tinashe-gutu <tinashegutu2011@gmail.com>
2022-03-13 18:59:08 +0000

    Creating array of random parts of the message

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,5 +1,5 @@
 
-const generateRandomMessage = () => {
+const generateRandomMessage = (num) => {
 
 
 }
@@ -9,3 +9,19 @@ const mixedMessages = {
     affirmative: [['you will'],['you wont'], ['you might'] ],
     event: [['fall in love.'], ['recieve a once in a life time offer.'], ['get arrested.']]
 }
+
+const randomNum = () => {
+    num = Math.floor((Math.random())*3)
+    return num
+}
+
+const createMessage = () =>{
+    const arrOfMessage = []
+    const when = mixedMessages.time[randomNum()];
+    console.log(when)
+    const affirm = mixedMessages.affirmative[randomNum()];
+    const what = mixedMessages.event[randomNum()];
+    arrOfMessage.push(when,affirm,what);
+    return arrOfMessage;
+}
+console.log(createMessage());
